6a5739358d8fa293e7e78fd11b8f4b10e32f49c6
Daniel Seidel <daniel.seidel@dlr.de>
2024-09-24 10:31:09 +0200

    feat: conan2 compatibility

--- conanfile.py
+++ conanfile.py
@@ -1,14 +1,15 @@
 from conan import ConanFile
 
 class MainProject(ConanFile):
-    python_requires = "conan_template/[~=5]@robotkernel/stable"
+    python_requires = "conan_template/[~5]@robotkernel/stable"
     python_requires_extend = "conan_template.RobotkernelConanFile"
+
     name = "module_pd_preprocessor"
     description = "module_pd_preprocessor is used to generate deterministic triggers for other modules."
     exports_sources = ["*", "!.gitignore"]
 
     def requirements(self):
-        self.requires("robotkernel/[~=5]@robotkernel/stable")
-        self.requires("service_provider_process_data_inspection/[~=5]@robotkernel/stable")
-        self.requires("service_provider_key_value/[~=5]@robotkernel/stable")
+        self.requires("robotkernel/[~5]@robotkernel/stable")
+        self.requires("service_provider_process_data_inspection/[~5]@robotkernel/stable")
+        self.requires("service_provider_key_value/[~5]@robotkernel/stable")
 
