0da8accf9675f44644bc9ef4027019158c49cc91
Robert Burger <robert.burger@dlr.de>
2026-05-26 11:47:22 +0200

    change: using stable deps

--- conanfile.py
+++ conanfile.py
@@ -1,7 +1,7 @@
 from conan import ConanFile
 
 class MainProject(ConanFile):
-    python_requires = "conan_template/[~5]@robotkernel/stable"
+    python_requires = "conan_template/[~6]@robotkernel/stable"
     python_requires_extend = "conan_template.RobotkernelConanFile"
 
     name = "module_pdpreprocessor"
@@ -9,10 +9,7 @@ class MainProject(ConanFile):
     exports_sources = ["*", "!.gitignore"]
 
     def requirements(self):
-        self.requires("robotkernel/[~6]@robotkernel/unstable")
-        self.requires("service_provider_process_data_inspection/[~6]@robotkernel/unstable")
-        self.requires("service_provider_key_value/[~6]@robotkernel/unstable")
-
-    def source(self):
-        self.run(f"sed 's/AC_INIT(.*/AC_INIT([{self.name}], [{self.version}], [{self.author}])/' configure.ac.in > configure.ac")
+        self.requires("robotkernel/[~6]@robotkernel/stable")
+        self.requires("service_provider_process_data_inspection/[~6]@robotkernel/stable")
+        self.requires("service_provider_key_value/[~6]@robotkernel/stable")
 
